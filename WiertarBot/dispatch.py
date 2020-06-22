d9cf86160b6434f6d9c3481c0f674e8ce2f156fc
Szczepan <szczepan7700@gmail.com>
2020-06-22 18:35:58 +0200

    Add EventDispatcher

---
+++ WiertarBot/dispatch.py
@@ -0,0 +1,22 @@
+class EventDispatcher():
+    _slots = {}
+
+    @staticmethod
+    def slot(event):
+        def wrap(func):      
+            name = event.__name__
+
+            if name not in EventDispatcher._slots:
+                EventDispatcher._slots[name] = []
+
+            EventDispatcher._slots[name].append(func)
+
+            return func
+        return wrap
+
+    @staticmethod
+    async def send_signal(event):
+        name = event.__name__
+        if name in EventDispatcher._slots:
+            for func in EventDispatcher._slots[name]:
+                await func(event)
