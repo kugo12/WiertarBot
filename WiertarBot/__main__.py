fd90669472d0d0fb0383be5f47fd91b3ccc21ec1
Szczepan Wiśniowski <szczepan7700@gmail.com>
2022-10-15 20:12:35 +0200

    Fix __main__.py

--- WiertarBot/__main__.py
+++ WiertarBot/__main__.py
@@ -9,20 +9,19 @@ from .integrations.health import startup_complete
 
 async def main() -> None:
     log.info("Starting initialization...")
-    await config.init()
-    # bot = await WiertarBot.create()
 
-    # atexit.register(bot.save_cookies)
-    # asyncio.get_running_loop().create_task(
-    #     WiertarBot.message_garbage_collector()
-    # )
+    await config.init()
+    bot = await WiertarBot.create()
+
+    atexit.register(bot.save_cookies)
+    asyncio.get_running_loop().create_task(
+        WiertarBot.message_garbage_collector()
+    )
 
     startup_complete()
+
     log.info("Initialization complete")
 
-    # await bot.run()
-
-    while True:
-        await asyncio.sleep(3600)
+    await bot.run()
 
 asyncio.run(main())
