969f06cf48bbdb8466af133a2dfa9666d099ad6d
Szczepan Wiśniowski <szczepan7700@gmail.com>
2023-04-09 22:40:39 +0200

    fix(fb): Get content_type from response in FBContext.upload

--- src/main/kotlin/pl/kvgx12/wiertarbot/connector/UploadedFile.kt
+++ src/main/kotlin/pl/kvgx12/wiertarbot/connector/UploadedFile.kt
@@ -4,4 +4,8 @@ data class UploadedFile @JvmOverloads constructor(
     val id: String,
     val mimeType: String,
     val content: ByteArray? = null,
-)
+) {
+    override fun toString(): String {
+        return "UploadedFile(id='$id', mimeType='$mimeType')"
+    }
+}
