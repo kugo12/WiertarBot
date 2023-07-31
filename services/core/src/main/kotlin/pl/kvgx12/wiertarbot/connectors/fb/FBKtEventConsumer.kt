62891bf5764516bc153a2c8fb974e0973ea68975
Szczepan Wiśniowski <szczepan7700@gmail.com>
2023-07-31 22:52:46 +0200

    refactor: nuke rabbitmq fb event publishing

--- services/core/src/main/kotlin/pl/kvgx12/wiertarbot/connectors/fb/FBKtEventConsumer.kt
+++ services/core/src/main/kotlin/pl/kvgx12/wiertarbot/connectors/fb/FBKtEventConsumer.kt
@@ -2,8 +2,6 @@ package pl.kvgx12.wiertarbot.connectors.fb
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.buildJsonObject
-import kotlinx.serialization.json.put
 import org.springframework.transaction.reactive.TransactionalOperator
 import org.springframework.transaction.reactive.executeAndAwait
 import pl.kvgx12.fbchat.data.events.Event
@@ -14,12 +12,10 @@ import pl.kvgx12.fbchat.session.Session
 import pl.kvgx12.wiertarbot.entities.FBMessage
 import pl.kvgx12.wiertarbot.repositories.FBMessageRepository
 import pl.kvgx12.wiertarbot.services.PermissionService
-import pl.kvgx12.wiertarbot.services.RabbitMQService
 import pl.kvgx12.wiertarbot.utils.getLogger
 
 class FBKtEventConsumer(
     private val permissionService: PermissionService,
-    private val rabbitMQService: RabbitMQService,
     private val fbMessageRepository: FBMessageRepository,
     private val fbMilestoneTracker: FBKtMilestoneTracker,
     private val transaction: TransactionalOperator,
@@ -68,14 +64,6 @@ class FBKtEventConsumer(
     private suspend fun consumeMessage(session: Session, event: ThreadEvent.WithMessage) {
         fbMilestoneTracker.events.send(session to event)
 
-        val serialized = FBMessageSerialization.serializeMessageEvent(event)
-
-        runCatching {
-            rabbitMQService.publishMessageEvent(serialized)
-        }.onFailure {
-            log.error("Failed to publish message to RabbitMQ", it)
-        }
-
         transaction.executeAndAwait {
             fbMessageRepository.save(
                 FBMessage(
@@ -83,26 +71,13 @@ class FBKtEventConsumer(
                     threadId = event.thread.id,
                     authorId = event.author.id,
                     time = event.message.createdAt ?: 0,
-                    message = serialized,
+                    message = FBMessageSerialization.serializeMessageEvent(event),
                 ),
             )
         }
     }
 
     private suspend fun unsendMessage(event: ThreadEvent.UnsendMessage) {
-        runCatching {
-            rabbitMQService.publishMessageDelete(
-                buildJsonObject {
-                    put("message_id", event.message.id)
-                    put("thread_id", event.thread.id)
-                    put("author_id", event.author.id)
-                    put("at", event.timestamp)
-                }.toString(),
-            )
-        }.onFailure {
-            log.error("Failed to publish message delete to RabbitMQ", it)
-        }
-
         transaction.executeAndAwait {
             fbMessageRepository.markDeleted(event.message.id, event.timestamp)
         }
