72ba4abe866ae4fb1157cc585263230d5f35f9b4
Szczepan Wiśniowski <szczepan7700@gmail.com>
2023-08-06 17:29:30 +0200

    chore(libs/core-proto): add ConnectorContext grpc service definition

--- libs/core-proto/build.gradle.kts
+++ libs/core-proto/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.protobuf)
@@ -6,8 +8,31 @@ plugins {
 dependencies {
     api(libs.protobuf.kotlin)
     api(libs.protobuf.java)
+    api(libs.grpc.kotlin.stub)
+    api(libs.grpc.protobuf)
 }
 
-protobuf.generateProtoTasks.all().configureEach {
-    plugins.register("kotlin")
+protobuf {
+    generateProtoTasks.all().configureEach {
+        plugins {
+            id("grpc")
+            id("grpckt")
+        }
+        builtins {
+            id("kotlin")
+        }
+    }
+
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+
+    plugins {
+        id("grpc") {
+            artifact = libs.grpc.protoc.java.get().toString()
+        }
+        id("grpckt") {
+            artifact = libs.grpc.protoc.kotlin.get().toString() + ":jdk8@jar"
+        }
+    }
 }
