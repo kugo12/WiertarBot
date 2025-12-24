a03c7abbc7eb109de6695df7cbd8ddb3bcc560c9
Szczepan Wiśniowski <15050771+kugo12@users.noreply.github.com>
2025-12-24 10:05:29 +0100

    add exception logging for grpc calls

--- libs/connector/src/main/kotlin/pl/kvgx12/wiertarbot/connector/Config.kt
+++ libs/connector/src/main/kotlin/pl/kvgx12/wiertarbot/connector/Config.kt
@@ -1,11 +1,13 @@
 package pl.kvgx12.wiertarbot.connector
 
 import io.grpc.BindableService
+import io.grpc.Server
 import io.grpc.ServerBuilder
 import org.springframework.beans.factory.BeanRegistrarDsl
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
+import pl.kvgx12.wiertarbot.connector.utils.getLogger
 
 class ConnectorBeanRegistrar : BeanRegistrarDsl({
     registerBean<RabbitMQ>()
@@ -19,9 +21,15 @@ data class ConnectorProperties(val port: Int = 8080)
 
 @EnableConfigurationProperties(ConnectorProperties::class)
 class ConnectorConfiguration(private val props: ConnectorProperties) {
+    private val log = getLogger()
+
     @Bean
-    fun connectorServer(services: List<BindableService>) =
-        ServerBuilder.forPort(props.port)
+    fun connectorServer(services: List<BindableService>): Server {
+        log.info("Starting Connector gRPC server on port ${props.port}")
+
+        return ServerBuilder.forPort(props.port)
+            .intercept(GrpcExceptionInterceptor())
             .addServices(services.map { it.bindService() })
             .build()
+    }
 }
